32f1efdd6e638c80e6c4c4155474480d21f02531
John Singer <johnsinger@cmail.carleton.ca>
2021-05-07 11:01:23 -0400

    implementation of the client server communiations

---
+++ Client.py
@@ -0,0 +1,59 @@
+from socket import *
+import sys
+import struct
+
+# helper functions to create messages and to split messages
+
+def createMessage(seq, msg):
+    return str(seq) + "@" + msg
+
+def splitMessage(input):
+    return (input[:input.find('@')], input[input.find('@')+1:])
+
+# Get the server hostname and port as command line arguments
+argv = sys.argv                      
+if len(argv ) != 3:
+    print("Incorrect number of command-line arguments")
+    print("Invoke client with: python", argv[0], "<MulticastIP> <port>")
+    exit()
+
+host = argv[1]
+port = int(argv[2])
+
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+# Set socket timeout as 1 second
+clientSocket.settimeout(1)
+
+# Set the time-to-live for messages to 1 so they do not go past the
+# local network segment.
+ttl = struct.pack('b', 1)
+clientSocket.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, ttl)
+
+# we will get a (potentially) large number of replies for each request
+# add a sequence number to the messages and read replies until we get
+# to the first one with a new sequence number
+
+msg_sequence = 0
+
+while True:
+
+    msg_sequence += 1
+    message = createMessage(msg_sequence, input("Next command: "))
+    
+    try:
+        clientSocket.sendto(message.encode(), (host, port))
+        reply, serverAddress = clientSocket.recvfrom(2048)
+        (seq, reply) = splitMessage(reply.decode())
+        while int(seq) < msg_sequence:
+            # print("Discard old reply")
+            reply, serverAddress = clientSocket.recvfrom(2048)
+            (seq, reply) = splitMessage(reply.decode())
+
+        print(reply)
+        if reply == "bye":
+            break
+    except:
+        print("Message to server timed out, please retry")
+
+# once we break out of the loop, close socket and terminate
+clientSocket.close()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Client-Server
 Client server interactions using multithreading and UDP communication
+A client suerver room reservation application that can support multiple clients.
+server reads and writes to rooms.txt
